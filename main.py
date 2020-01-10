69347a7d3db7e57027e297d5d2019f8aad4264c5
vin tang <vincent1@Admins-MacBook-Pro-8.local>
2020-01-10 14:28:00 -0500

    transfer learning with resnet18

--- main.py
+++ main.py
@@ -5,62 +5,68 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import numpy as np
-from model import Net
+import time
+from torchvision import models
 
 # check for gpu
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print("device: ", device)
+print(">> device check: ", device)
 
+print(">> preparing data...")
 # define a transformation pipeline for our raw data
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+transform = transforms.Compose([transforms.RandomResizedCrop(224), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 # download raw data and then apply transformation pipeline
-print("downloading training data...")
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 
 # then construct a generator from the transformed data
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True)
-
-# instantiate a model according to that architecture
-print("instantiating model...")
-net = Net().to(device)  # send it to gpu
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-
-n_epochs = 100
-
-print("training")
-for epoch in range(n_epochs):
-
-    for batch_idx, data in enumerate(trainloader):
-
-        X, y = data[0].to(device), data[1].to(device)  # need to send data to gpu too, not just the model
-
-        optimizer.zero_grad()  # clean up gradients
-        outputs = net(X)  # emit class logits
-        loss = criterion(outputs, y)  # compute loss
-        loss.backward()  # compute gradients wrt loss
-        optimizer.step()  # update weights according to computed gradients
-
-        # print statistics
-        if batch_idx % 500 == 0:
-            print("epoch: %d loss: %.3f" % (epoch, loss.item()))
-print('Finished Training')
-
-print("downloading test data...")
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=500, shuffle=True)
 
-print("testing")
-net.eval()
-for data in testloader:
-    
-    X, y = data[0].to(device), data[1].to(device)
-    
-    output = net(X)
-    pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+# fetch resnet 18
+print(">> building model")
+model_ft = models.resnet18(pretrained=True).to(device)  # send it to gpu
+num_ftrs = model_ft.fc.in_features
+num_classes = 10  # cifar10 has 10 classes
+model_ft.fc = nn.Linear(num_ftrs, num_classes)
+criterion = nn.CrossEntropyLoss()
+optimizer_ft = optim.SGD(model_ft.parameters(), lr=1e-3, momentum=0.9)
 
-    correct = 0
-    correct += pred.eq(y.view_as(pred)).sum().item()
-    print(correct / len(X))
+n_epochs = 5
+
+print(">> training loop")
+for epoch in range(n_epochs):
+
+    start = time.time()
+    
+    # train phase
+    model_ft.train()
+    for batch_idx, data in enumerate(trainloader):
+
+        X = data[0].to(device) 
+        y = data[1].to(device)
+
+        optimizer_ft.zero_grad()  # clean up gradients
+        outputs = model_ft(X)  # emit class logits
+        loss = criterion(outputs, y)  # compute loss
+        loss.backward()  # compute gradients wrt loss
+        optimizer_ft.step()  # update weights according to computed gradients
+
+        # print statistics
+        if batch_idx % 10 == 0:
+            print("epoch: %d loss: %.3f" % (epoch, loss.item()))
+
+
+    # validate phase
+    model_ft.eval()
+    running_corrects = 0
+    for X, y in testloader:
+        output = model_ft(X)
+        preds = output.argmax(dim=1, keepdim=False)  # get predictions from argmax
+        running_corrects += torch.sum(preds == y)
+    epoch_acc = running_corrects.double() / len(testset)
+    print("accuracy", epoch_acc.item())
+
+    time_elapsed = time.time() - start
+    print("epoch run time", time_elapsed)
--- model.py
+++ model.py
@@ -6,38 +6,9 @@ import torch.optim as optim
 import torch.nn.functional as F
 import numpy as np
 
-class LinearLayer(nn.Module):
-    def __init__(self, in_sz, out_sz):
-        super().__init__()
-        t1 = torch.randn(in_sz, out_sz)
-        self.w = nn.Parameter(t1)
-        t2 = torch.randn(out_sz)
-        self.b = nn.Parameter(t2)
-        
-    def forward(self, activations):
-        t = torch.mm(activations, self.w)
-        return t + self.b
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()  # this is how objects that inherit from parent classes are instantiated
-        
-        # layers are nodes that are stateful
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = LinearLayer(120, 84)  # lets use our custom layer
-        self.fc3 = nn.Linear(84, 32)
-        self.fc4 = nn.Linear(32, 10)
-
-    def forward(self, x):
-        # think of these as the edges; they are transformations and are therefore stateless
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = self.fc4(x)  # outputs raw logits rather than class probabilities
-        return x
+# fetch pretrained resnet
+model_ft = models.resnet18(pretrained=True)
+num_ftrs = model_ft.fc.in_features
+num_classes = 10  # cifar 10
+model_ft.fc = nn.Linear(num_ftrs, num_classes)
