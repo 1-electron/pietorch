67d3b1514a19da539db776e8f43a4a485e01acce
mynameisvinn <vin.tang@gmail.com>
2020-01-18 21:28:02 -0500

    relu operation

---
+++ nn/Relu.py
@@ -0,0 +1,24 @@
+import autograd.numpy as np 
+from autograd import grad, elementwise_grad
+from .Tensor import Tensor
+
+class Relu(object):
+    def __new__(self, x):
+        op = _Relu(x)
+        name = "Relu"
+        return Tensor(val=op.evaluate(), parents=[x], 
+                      op=op, terminal=False, name=name)
+        
+class _Relu(object):
+    def __init__(self, x):
+        self.x = float(x.val)
+        
+    def evaluate(self):
+        return max(0, self.x)
+
+    def compute_parents_grads(self):
+        if self.x > 0:
+            self.df_dx = 1
+        else:
+            self.df_dx = 0
+        return [self.df_dx]
--- nn/__init__.py
+++ nn/__init__.py
@@ -2,3 +2,4 @@ from .Tensor import Tensor
 from .Add import Add
 from .Multiply import Multiply
 from .Pow import Pow
+from .Relu import Relu
