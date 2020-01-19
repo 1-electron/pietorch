af0c013f19af74becb884525535f28cd31a8e5b0
mynameisvinn <vin.tang@gmail.com>
2020-01-18 19:37:12 -0500

    add exponent as an operation

--- Op.py
+++ Op.py
@@ -2,6 +2,32 @@ import autograd.numpy as np
 from autograd import grad, elementwise_grad
 from Tensor import Tensor
 
+
+class Pow(object):
+    def __new__(self, x, y):
+        op = _Power(x, y)
+        name = "Power"
+        return Tensor(val=op.evaluate(), parents=[x], 
+                      op=op, terminal=False, name=name)
+        
+class _Power(object):
+    def __init__(self, x, y):
+        self.x = float(x.val)
+        self.y = float(y)  # unlike other operations, y is not a tensor
+        
+    def evaluate(self):
+        return self.x ** self.y
+
+    def f(self, x, y):
+        return x ** y
+
+    def compute_parents_grads(self):
+        # return a list of gradients
+        self.df_dx = elementwise_grad(self.f, 0)
+        return [self.df_dx(self.x, self.y)]
+
+
+
 """
 Add is an operation. when Add is called, an Adder object is 
 created and used to instantiate a Tensor object, which is 
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # pietorch
 a python implementation of pytorch. in a nutshell, it is autograd + graphs.
+
+## example
+```python
+from Tensor import Tensor
+from Op import Pow
+
+X = Tensor(val=10, name="X")
+Z = Pow(X, 2)
+Z.val  # 100
+Z.backward()  # compute gradients
+X.grad  # gradient is 2x = 20
+```
